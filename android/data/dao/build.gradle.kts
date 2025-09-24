34eaa0c08670a8cf9fcbac1c129b176497b1a8e7
czy! <1322596900@qq.com>
2025-09-24 23:19:48 +0800

    feat(core): 添加模块间依赖关系- 在 appcore 模块中添加对 domain 模块的依赖 - 在 dao 模块中添加对 domain、baseutil 和 appcore 模块的依赖 - 在 domain 模块中添加对 baseutil 模块的依赖 - 在 appview 模块中添加对 domain、baseutil 和 appcore 模块的依赖

--- android/data/dao/build.gradle.kts
+++ android/data/dao/build.gradle.kts
@@ -40,4 +40,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(project(":data:domain"))
+    implementation(project(":core:baseutil"))
+    implementation(project(":core:appcore"))
 }
