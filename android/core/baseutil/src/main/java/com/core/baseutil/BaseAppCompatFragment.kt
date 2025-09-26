9e50fb280537bf67d836f5221808fa6d6cb005a1
chenzy <1322596900@qq.com>
2025-09-26 16:29:22 +0800

    feat(core): 添加 BaseAppCompatFragment 基类- 创建抽象基类 BaseAppCompatFragment用于 Fragment 管理- 集成 ViewBinding 支持，简化视图绑定操作 - 提供 onCreateView 和 onViewCreated 生命周期封装 - 添加 initView 和 setListener 抽象方法供子类实现- 支持通过泛型指定 Fragment 类型并生成 tag 标识 - 实现 binding 延迟初始化和自动绑定 rootView

---
+++ android/core/baseutil/src/main/java/com/core/baseutil/BaseAppCompatFragment.kt
@@ -0,0 +1,48 @@
+package com.core.baseutil
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+import kotlin.reflect.KClass
+
+abstract class BaseAppCompatFragment<VB : ViewBinding>(
+    fragmentClassType: KClass<out Fragment>
+) : Fragment(){
+
+    protected lateinit var binding: VB
+    abstract fun initBinding(): VB
+    protected val tag = fragmentClassType.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = initBinding()
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // 此处binding才生效
+        initView()
+
+        setListener()
+    }
+
+    protected open fun initView(){
+
+    }
+
+    protected open fun setListener() {
+
+    }
+
+}
