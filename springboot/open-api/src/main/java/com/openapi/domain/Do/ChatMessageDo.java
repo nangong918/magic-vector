06767c3a65699da786ba60a425578caeaf303817
chenzy <1322596900@qq.com>
2025-09-30 16:08:40 +0800

    feat(chat): 新增聊天消息实体类及数据访问层实现
    新增 ChatMessageDo 实体类，用于表示聊天消息数据结构。
    新增 ChatMessageMapper 接口及对应的 MyBatis XML 映射文件，实现聊天消息的增删查功能，支持批量操作和按条件查询历史消息。
    
    此外，在设计文档中补充了关于分表存储聊天记录的后续优化思路。

---
+++ springboot/open-api/src/main/java/com/openapi/domain/Do/ChatMessageDo.java
@@ -0,0 +1,21 @@
+package com.openapi.domain.Do;
+
+import cn.hutool.core.util.IdUtil;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+
+@Data
+public class ChatMessageDo {
+    @Id
+    private String id = String.valueOf(IdUtil.getSnowflake().nextId());
+    private String agentId; // 也可也理解为sessionId, 毕竟救我一个用户
+//    private String userId; // 就老子一个用户
+    private String content;
+    // 时间
+    private LocalDateTime chatTime;
+    // 发送方: 0: agent, 1: user
+    private Integer role = 0;
+}
