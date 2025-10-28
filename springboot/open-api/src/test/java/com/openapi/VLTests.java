fdf86edfd4e62587c90201b8bb8bdf2809c20cf8
czy! <1322596900@qq.com>
2025-10-28 22:11:26 +0800

    test(vl): 增加VL测试todo

---
+++ springboot/open-api/src/test/java/com/openapi/VLTests.java
@@ -0,0 +1,17 @@
+package com.openapi;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Qwen VL 视觉理解测试
+ * 参考网址：<a href="https://help.aliyun.com/zh/model-studio/vision?spm=a2c4g.11186623.help-menu-2400256.d_0_2_0.24886be9vqRmce&scm=20140722.H_2845871._.OR_help-T_cn~zh-V_1#071b239d9371c">...</a>
+ */
+@Slf4j
+@SpringBootTest(classes = MainApplication.class)
+public class VLTests {
+
+
+
+}
