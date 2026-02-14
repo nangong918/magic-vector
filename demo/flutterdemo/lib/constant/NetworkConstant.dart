8cc5a749bf59724824cc9e6aa3da569c6042821f
chenzy <Lemon.chen@lednets.com>
2026-02-14 10:00:04 +0800

    refactor: 删除老版本的flutter

--- demo/flutterdemo/lib/constant/NetworkConstant.dart
+++
@@ -1,9 +0,0 @@
-class NetworkConstant {
-  const NetworkConstant._();
-
-  static const String dns = '192.168.3.140';
-  static const int port = 58888;
-  static const String protocol = 'http';
-
-  static String get baseUrl => '$protocol://$dns:$port';
-}
--- demo/flutterdemo/lib/network/ApiRequest.dart
+++
@@ -1,9 +0,0 @@
-import '../domain/dto/BaseResponse.dart';
-import '../domain/dto/req/UserTestReq.dart';
-import '../domain/dto/resp/UserTestResp.dart';
-
-abstract class ApiRequest {
-  Future<BaseResponse<UserTestResp>> register(UserTestReq req);
-
-  Future<BaseResponse<UserTestResp>> resetToken(String account);
-}
