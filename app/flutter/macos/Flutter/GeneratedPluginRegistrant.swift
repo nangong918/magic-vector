827d046dd5cd3995eb3d1b2f2c434a27877239bf
chenzy <Lemon.chen@lednets.com>
2026-02-25 11:00:16 +0800

    feat: 基本依赖迁移

--- app/flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
+import record_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
 }
