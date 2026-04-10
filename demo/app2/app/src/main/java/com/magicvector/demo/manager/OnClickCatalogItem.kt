87b1e5feddfdd854c8d20a6c62e6ad7c26034a21
czy! <1322596900@qq.com>
2026-04-10 11:45:16 +0800

    删除老版本app

--- demo/app2/app/src/main/java/com/magicvector/demo/manager/OnClickCatalogItem.kt
+++
@@ -1,7 +0,0 @@
-package com.magicvector.demo.manager
-
-import com.magicvector.demo.domain.vo.CatalogItem
-
-interface OnClickCatalogItem {
-    fun onClick(item: CatalogItem)
-}
--- demo/app2/app/src/main/java/com/magicvector/demo/utils/sort/SortItem.kt
+++
@@ -1,5 +0,0 @@
-package com.magicvector.demo.utils.sort
-
-interface SortItem {
-    fun getIndex(): Long
-}
