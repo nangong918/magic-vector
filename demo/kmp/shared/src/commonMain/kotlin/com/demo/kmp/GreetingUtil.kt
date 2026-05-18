da6cda0e162de3d436d7a26e18722d7ed7a5f9bd
czy! <1322596900@qq.com>
2026-05-18 09:48:04 +0800

    feat: Add Kotlin cross-platform(KMP) support

---
+++ demo/kmp/shared/src/commonMain/kotlin/com/demo/kmp/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.demo.kmp
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ demo/kmp/shared/src/iosMain/kotlin/com/demo/kmp/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.demo.kmp
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
